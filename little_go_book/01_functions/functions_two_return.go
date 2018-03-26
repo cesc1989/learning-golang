72538803242c755f400ebbfeae3630415c481dac
Francisco Quintero <frajaquico@yahoo.es>
2018-03-26 15:37:13 -0500

    Comments for functions

--- little_go_book/01_functions/functions_two_return.go
+++ little_go_book/01_functions/functions_two_return.go
@@ -3,8 +3,11 @@ package main
 import "fmt"
 
 func main() {
+  // functions can have two return values
   lad, result := do_it("yamcha")
 
+  // use the boolean expresion of one the returned values
+  // to make the conditional look better
   if result {
     fmt.Println(lad)
   } else {
@@ -12,8 +15,10 @@ func main() {
   }
 }
 
+// return values specified in return options of the function: a string and a boolean
 func do_it(s string) (string, bool) {
   if s == "yamcha" {
+    // return the values separated by comma
     return "Mocoro", true
   }else if s == "gohan" {
     return "Mocoro", false

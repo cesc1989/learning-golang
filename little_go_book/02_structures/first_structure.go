8b2d125dc3c66c834a3cd2977dcc48fcb729257f
Francisco Quintero <frajaquico@yahoo.es>
2018-03-26 17:42:30 -0500

    More description comments to first_structure.go

--- little_go_book/02_structures/first_structure.go
+++ little_go_book/02_structures/first_structure.go
@@ -9,6 +9,7 @@ func main(){
       goku := Saiyan{Name: "Goku"}
       goku.Power = 9000
   */
+  // note the trailing comma. It is required for this code to compile
   goku := Saiyan{
     Name: "Goku",
     Power: 9000,
@@ -16,22 +17,29 @@ func main(){
 
   // Go passes arguments by value(copy of variable) not by reference(original variable)
   Super(goku)
-  fmt.Println(goku.Power) // prints 9000
+  fmt.Println(goku.Power) // prints 9000, not 19000
 
+  // You can define the values of the structure basing in the order they're defined
   gohan := &Saiyan{"Gohan", 3000}
   super_pointer(gohan)
-  fmt.Println(gohan.Power)
+  fmt.Println(gohan.Power) // prints 13000, not 3000
 }
 
+// Go does not have classes but you can define structures using the struct keyword
 type Saiyan struct {
   Name string
   Power int
 }
 
+// then things can be declared as being of type of given structured
+// in this case, variable `s` type is Saiyan
 func Super(s Saiyan){
   s.Power += 10000
 }
 
+// As Go passes arguments by value and not reference, we can use Pointers to accomplish the latter
+// You need to use the _address of_ operator (&) and the _pointer to_ (*)
+// with &StructName (&Saiyan, in line 23) you can get the address in memory of the given variable value
 func super_pointer(s *Saiyan){
   s.Power += 10000
 }
